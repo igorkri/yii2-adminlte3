5a13c512b790053943414db700968a40f8b53d51
igorkri <igorkri26@gmail.com>
2023-06-06 14:37:44 +0300

    test 2

--- src/views/site/index.php
+++ src/views/site/index.php
@@ -9,11 +9,11 @@ $this->params['breadcrumbs'] = [['label' => $this->title]];
             //     'type' => 'success',
             //     'body' => '<h3>Congratulations!</h3>',
             // ]) ?>
-            <?= \igorkri\adminlte\widgets\Callout::widget([
-                'type' => 'danger',
-                'head' => 'I am a danger callout!',
-                'body' => 'There is a problem that we need to fix. A wonderful serenity has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with my whole heart.'
-            ]) ?>
+            <?php // \igorkri\adminlte\widgets\Callout::widget([
+            //     'type' => 'danger',
+            //     'head' => 'I am a danger callout!',
+            //     'body' => 'There is a problem that we need to fix. A wonderful serenity has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with my whole heart.'
+            // ]) ?>
         </div>
     </div>
 
