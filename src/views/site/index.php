a130f7048a31fa6320f5f244f589d6d9956b40e1
igorkri <igorkri26@gmail.com>
2023-06-06 14:21:35 +0300

    test

--- src/views/site/index.php
+++ src/views/site/index.php
@@ -5,10 +5,10 @@ $this->params['breadcrumbs'] = [['label' => $this->title]];
 <div class="container-fluid">
     <div class="row">
         <div class="col-lg-6">
-            <?= \igorkri\adminlte\widgets\Alert::widget([
-                'type' => 'success',
-                'body' => '<h3>Congratulations!</h3>',
-            ]) ?>
+            <?php // \igorkri\adminlte\widgets\Alert::widget([
+            //     'type' => 'success',
+            //     'body' => '<h3>Congratulations!</h3>',
+            // ]) ?>
             <?= \igorkri\adminlte\widgets\Callout::widget([
                 'type' => 'danger',
                 'head' => 'I am a danger callout!',
